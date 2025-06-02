4d7c868e332418fdd2542d81a5aebce813601a92
Jose Monge <josfi0427@gmail.com>
2025-06-01 18:44:24 -0600

    Changes requested by Eduardo

--- semana 3/ejercicio2.py
+++
@@ -1,6 +0,0 @@
-num_1 = 1 + 1
-num_2 = 90 + 430
-num_3 = 4 * 5
-print(num_1)
-print(num_2)
-print(num_3)
--- semana11/ejercicio 3/main.py
+++
@@ -1,6 +0,0 @@
-import menu
-
-print("Bienvenid@!")
-code = menu.form_options()
-
-print(code)
