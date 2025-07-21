849b1d09ebc5161c40def3d1d4ccd402ec146fb9
josei0427 <josfi0427@gmail.com>
2025-07-20 21:19:39 -0600

    Delete MODULE2/FRONTEND directory

--- MODULE2/FRONTEND/semana5/ejercicio1.js
+++
@@ -1,4 +0,0 @@
-const myArray = ["Alek", "Jose", "Alex", "Eduardo"]
-for (let i = 0; i < myArray.length; i++) {
-    console.log(myArray[i]);
-}
--- MODULE2/FRONTEND/semana5/ejercicio3.js
+++
@@ -1,6 +0,0 @@
-const myTempC = [27, 19, 30, 22, 32];
-let myTempF = myTempC.map(function(i){
-    return (i * 9 / 5) + 32
-})
-
-console.log(myTempF)
